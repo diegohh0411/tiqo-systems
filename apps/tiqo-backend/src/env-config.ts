e6d0fedc36c17a15ec6ab6ccb0b393884d428a06
Diego Hernández Herrera <diegohh0411@gmail.com>
2025-05-14 12:33:43 -0600

    fix: add NODE_VERSION env variable for Render

--- apps/tiqo-backend/src/env-config.ts
+++ apps/tiqo-backend/src/env-config.ts
@@ -12,6 +12,8 @@ const envSchema = z.object({
   DATABASE_URL: z.string(),
 
   COOKIE_SECRET: z.string().min(32).max(100),
+
+  NODE_VERSION: z.string().regex(/^\d+\.\d+\.\d+$/).default('20.19.0'),
 });
 
 const env = envSchema.safeParse(process.env);
